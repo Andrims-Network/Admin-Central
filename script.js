5023d85b1b1012cebcd7369d3fe9366646be9f0c
Aulteran <aadilmirhussain@gmail.com>
2022-06-27 23:20:31 -0400

    Initial Login Page
    Includes new Username and Password fields + in progress javascript login func

---
+++ script.js
@@ -0,0 +1,5 @@
+function login(){
+    var username = document.getElementById("usnm");
+    var password = document.getElementById("pwd");
+    document.getElementsByClassName("feedback") = username + " " + password;
+}
