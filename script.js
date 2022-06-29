744e0f355a6a08c2aceb53781a94084f803d43cc
Aulteran <aadilmirhussain@gmail.com>
2022-06-28 22:35:02 -0400

    Fixed Login Feature
    js now grabs credentials properly and uses it to set welcome text.

--- script.js
+++ script.js
@@ -2,24 +2,40 @@
 const usnmAult = "ault@andrims.com";
 const usnmBowswa = "bowswa@andrims.com";
 
+const pwdAult = "testpwd";
+const pwdBowswa = "temppwd";
+
 function login(){
     //get credentials
-    var username = document.getElementById("usnm");
-    var password = document.getElementById("pwd");
+    var username = document.getElementById("usnm").value;
+    var password = document.getElementById("pwd").value;
 
     //determine user
     if(username == usnmAult){
-        userAult();
-        mainUnlock();
+        if(password == pwdAult){
+            userAult();
+            mainUnlock();
+        } else{
+            window.alert("Wrong Password");
+        }
     }
 
     if(username == usnmBowswa){
-        mainUnlock();
+        if(password == pwdBowswa){
+            userBowswa();
+            mainUnlock();
+        } else{
+            window.alert("Wrong Password");
+        }
     }
 }
 
 function userAult(){
-    document.getElementById("welcomeUsername") = ", Ault";
+    document.getElementById("welcomeUsername").innerHTML = ", Ault";
+}
+
+function userBowswa(){
+    document.getElementById("welcomeUsername").innerHTML = ", Ault";
 }
 
 function mainUnlock(){
