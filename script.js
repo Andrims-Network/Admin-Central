6a51dc09ff06b48bb2d0e5f0d7a253698a114551
Aulteran <aadilmirhussain@gmail.com>
2022-06-29 14:16:21 -0400

    Deleted attempts at read and write to authentication files
    Disregarded everything in relation to fopen(), fread(), and fwrite(). Got confused, that's PHP, not JS.
    Also replicated userAult() to userBowswa()

--- script.js
+++ script.js
@@ -5,12 +5,6 @@ const usnmBowswa = "bowswa@andrims.com";
 const pwdAult = "testpwd";
 const pwdBowswa = "temppwd";
 
-//fopen([filepath], #)
-//number at and: 0 for read, 3 for write
-var aultVerification = fopen("/aultverify.txt",3);
-var bowswaVerification = fopen("bowswaverify.txt",3);
-var verification;
-
 function login(){
     //get credentials
     var username = document.getElementById("usnm").value;
@@ -38,9 +32,9 @@ function login(){
 function userAult(){
     document.getElementById("welcomeUsername").innerHTML = ", Ault";
     document.getElementById("continueButton").removeAttribute("hidden");
-    fwrite(aultVerification, "open");
 }
 
 function userBowswa(){
-    document.getElementById("welcomeUsername").innerHTML = ", Ault";
+    document.getElementById("welcomeUsername").innerHTML = ", Bowswa";
+    document.getElementById("continueButton").removeAttribute("hidden");
 }
