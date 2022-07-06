3bf210dd67ddd394c9bc557ab652d4dabfa1722d
HalfCookedRamen <75753005+the-ramenator@users.noreply.github.com>
2022-07-06 16:07:05 +0000

    bug fixes and restyle

--- bg.js
+++ bg.js
@@ -6,14 +6,16 @@ function freshDot(){
     this.obj.classList.add("box");
     this.obj.style.top = (window.innerHeight * Math.random()) + 'px';
     this.obj.style.left = (window.innerWidth * Math.random()) + 'px';
-  this.obj.style.animationDelay = random(0,5,2)+'s';
-    var reverse = random(1,2)
+
+  this.obj.style.animation = "hover "+random(5,20,2)+"s infinite";
+ this.obj.style.animationDelay = random(0,5,2)+'s';
+  var reverse = random(1,2,0);
+  
   if(reverse == 2){
     this.obj.style.animationDirection = "reverse";
   }
     this.obj.style.height =  random(10,200, 0) + 'px';
     this.obj.style.width = random(10,200, 0) + 'px';
-    // this.obj.style.transform = 'rotate('+random(-1,1)+'deg)';
     document.body.appendChild(this.obj);
   }
   var dot = [];
