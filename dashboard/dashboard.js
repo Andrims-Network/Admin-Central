a243a91b1560f8cb68fd831a06588caa1976994e
Ault <aadilmirhussain@gmail.com>
2022-07-13 07:44:23 +0000

    Styled StaffDir and created files/dir switcher with radios

---
+++ dashboard/dashboard.js
@@ -0,0 +1,9 @@
+function staffDirDisplay(){
+	document.getElementById('staffDir').style.display = 'block';
+	document.getElementById('files').style.display = 'none';
+}
+
+function filesDisplay(){
+	document.getElementById('staffDir').style.display = 'none';
+	document.getElementById('files').style.display = 'block';
+}
