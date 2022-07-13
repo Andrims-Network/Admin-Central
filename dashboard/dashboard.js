930bd2dc7deb7407939b115828f7f17399861759
HalfCookedRamen <75753005+the-ramenator@users.noreply.github.com>
2022-07-13 17:04:03 +0000

    added better buttons for files & staffdir (wip)

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,9 +1,14 @@
 function staffDirDisplay(){
 	document.getElementById('staffDir').style.display = 'block';
 	document.getElementById('files').style.display = 'none';
+  document.getElementById('btn-1').disabled = 'true';
+  document.getElementById('btn-2').removeAttribute("disabled");
 }
 
 function filesDisplay(){
 	document.getElementById('staffDir').style.display = 'none';
 	document.getElementById('files').style.display = 'block';
+  document.getElementById('btn-1').removeAttribute("disabled");
+  document.getElementById('btn-2').disabled = 'true';
 }
+staffDirDisplay();
